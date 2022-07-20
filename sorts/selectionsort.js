ddd86fbdca121c8a9007e95bb28cce8fe01d8df4
Justin Solo <justinpaulosolo@gmail.com>
2022-07-20 13:30:14 -0700

    ignore files

--- sorts/selectionsort.js
+++ sorts/selectionsort.js
@@ -4,9 +4,12 @@ function selectionSort (arr) {
     let length = arr.length;
 
     for(let i = 0; i < length; i++){
+        //assuming first num is the mininum
         let min = i;
 
         for(let j = i + 1; j < length; j++) {
+            // compare next num in arr if its lower
+            // if yes update min
             if (arr[j] < arr[min]) {
                 min=j;
             }
