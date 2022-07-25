d69322037049964a0d07b2a9e4e836aa49373962
Justin Solo <justinpaulosolo>
2022-07-24 17:07:41 -0700

    handle index collision

--- module_2/HashTable.js
+++ module_2/HashTable.js
@@ -15,13 +15,76 @@ class HashTable {
 
     set(key, value) {
         const index = this._hash(key);
-        this.table[index] = [key, value]
+        if (this.table[index]) {
+            for (let i = 0; i < this.table[index].length; i++) {
+                // Find the key/value pair in the chain
+                if (this.table[index][i][0] === key) {
+                    this.table[index][i][1] = value;
+                    return;
+                }
+            }
+            this.table[index].push([key, value]);
+        } else {
+            this.table[index] = [];
+            this.table[index].push([key, value]);
+        }
         this.size++;
     };
 
     get(key) {
-        const index = this._hash(key);
-        return this.table[index];
+        const target = this._hash(key);
+        if (this.table[target]) {
+            for (let i = 0; i < this.table[target].length; i++) {
+                if (this.table[target][i][0] === key) {
+                   return this.table[target][i][1];
+                }
+            }
+        }
+        return undefined;
     };
 
+    remove(key) {
+        const index = this._hash(key);
+        if (this.table[index] && this.table[index].length){
+            for (let i = 0; i < this.table.length; i++) {
+                if (this.table[index][i][0] === key) {
+                    this.table[index].splice(i, 1);
+                    this.size--;
+                    return true;
+                }
+            }
+        } else {
+            return false;
+        }
+    };
+
+    display() {
+        this.table.forEach((values, index) =>{
+            const chainedValues = values.map(
+                ([key, value]) => `[ ${key}: ${value} ]`
+            );
+            console.log(`${index}: ${chainedValues}`);
+        })
+    }
 }
+
+/*
+const ht = new HashTable();
+ht.set("Canada", 300);
+ht.set("France", 100);
+ht.set("Spain", 110);
+
+console.log(ht.get("Canada"));
+console.log(ht);
+ht.remove('Spain');
+console.log(ht);
+ */
+
+const ht = new HashTable();
+ht.set("Spain", 110);
+ht.set("ǻ", 192);
+
+console.log(ht.get("Spain"));
+console.log(ht.get("ǻ"));
+ht.display();
+
